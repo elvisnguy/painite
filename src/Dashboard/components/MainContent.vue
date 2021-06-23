85befd3e60f16aca5163fcad17cb44c09ca1a0fb
Thanh Nguy <vanthanhnguy2907@gmail.com>
2021-06-23 15:36:51 +0700

    fix css responsive MainContent

--- src/Dashboard/components/MainContent.vue
+++ src/Dashboard/components/MainContent.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-container style="padding: 5% 0% 0% 5% !important">
+    <v-container>
       <v-row>
         <router-view></router-view>
       </v-row>
@@ -12,4 +12,13 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.container {
+  padding: 5% 0% 0% 5%;
+}
+@media only screen and (max-width: 739px) {
+  .container {
+    padding: 15% 0% 0% 10%;
+  }
+}
+</style>
