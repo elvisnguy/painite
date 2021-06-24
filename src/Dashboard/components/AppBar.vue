869a6f5df761381e2ef953e9b288166ca2e810e0
Thanh Nguy <vanthanhnguy2907@gmail.com>
2021-06-24 14:40:14 +0700

    fix responsive general dashboard layout

--- src/Dashboard/components/AppBar.vue
+++ src/Dashboard/components/AppBar.vue
@@ -7,68 +7,67 @@
       style="height: 70px; padding: 10px 0px"
       id="appbar-res"
     >
-      <v-row>
-        <v-col cols="2"></v-col>
-        <v-col cols="10" class="d-flex">
-          <v-btn color="#f2b334" id="hide-menu">
-            <v-icon>{{ icons.mdiMenu }}</v-icon>
+      <v-btn color="#f5f5f5" id="hide-menu">
+        <v-icon color="#1F2939">{{ icons.mdiMenu }}</v-icon>
+      </v-btn>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="#f5f5f5"
+            v-btn
+            v-bind="attrs"
+            v-on="on"
+            class="logo-company"
+          >
+            <h5 style="color: #616e7c">Eztek Company</h5>
+            <div class="px-2"></div>
+            <v-icon color="#616e7c">{{ icons.mdiArrowDownDropCircle }}</v-icon>
           </v-btn>
-          <v-menu left bottom class="logo-company">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn v-btn v-bind="attrs" v-on="on" color="#f2b334">
-                <h5>Eztek Company</h5>
-                <div class="px-2"></div>
-                <v-icon>{{ icons.mdiArrowDownDropCircle }}</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item @click="() => {}">
-                <v-list-item-title>
-                  Eztek Company
-                  <v-divider />
-                  Company List
-                </v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-          <v-spacer></v-spacer>
-          <v-btn color="#f2b334" id="appear-option">
-            <v-icon>{{ icons.mdiDotsHorizontal }}</v-icon>
+        </template>
+        <v-list>
+          <v-list-item @click="() => {}">
+            <v-list-item-title>
+              Eztek Company
+              <v-divider />
+              Company List
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-spacer></v-spacer>
+      <v-btn color="#f5f5f5" id="appear-option">
+        <v-icon color="#1F2939">{{ icons.mdiDotsHorizontal }}</v-icon>
+      </v-btn>
+      <a href="" class="contact-us">Contact Us</a>
+      <div class="mx-1"></div>
+      <v-menu left bottom id="hide-option">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-btn
+            v-bind="attrs"
+            v-on="on"
+            color="#f5f5f5"
+            id="hide-option"
+          >
+            <h5 v-for="user in listUser" :key="user" style="color: #1f2933">
+              {{ user.name }}
+            </h5>
+            <div class="px-1"></div>
+            <v-icon color="#DDE2E9">{{ icons.mdiAccount }}</v-icon>
+            <div class="px-1"></div>
+            <v-icon color="#1F2939">{{ icons.mdiArrowDownDropCircle }}</v-icon>
           </v-btn>
-          <a href="" class="contact-us">Contact Us</a>
-          <div class="mx-1"></div>
-          <v-menu left bottom id="hide-option">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                v-btn
-                v-bind="attrs"
-                v-on="on"
-                color="#f2b334"
-                id="hide-option"
-              >
-                <h5 v-for="user in listUser" :key="user">{{ user.name }}</h5>
-                <div class="px-1"></div>
-                <v-icon>{{ icons.mdiAccount }}</v-icon>
-                <div class="px-1"></div>
-                <v-icon>{{ icons.mdiArrowDownDropCircle }}</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item
-                v-for="list in listMenu"
-                :key="list"
-                @click="() => {}"
-              >
-                <v-list-item-title>{{ list }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-          <div class="mx-1"></div>
-          <v-btn color="#f2b334" id="hide-option">
-            <v-icon>{{ icons.mdiLogoutVariant }}</v-icon>
-          </v-btn>
-        </v-col>
-      </v-row>
+        </template>
+        <v-list>
+          <v-list-item v-for="list in listMenu" :key="list" @click="() => {}">
+            <v-list-item-title>{{ list }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <div class="mx-1"></div>
+      <v-btn color="#f5f5f5" id="hide-option">
+        <v-icon color="#f2b334">{{ icons.mdiLogoutVariant }}</v-icon>
+      </v-btn>
     </v-app-bar>
   </div>
 </template>
@@ -119,6 +118,9 @@ export default {
 };
 </script>
 <style>
+.logo-company {
+  margin-left: 220px;
+}
 .v-breadcrumbs__item {
   font-weight: 500;
   color: #1f2933 !important;
@@ -162,8 +164,8 @@ export default {
   .v-btn {
     min-width: 0px !important;
   }
-  #appbar-res {
-    width: 100%;
+  .logo-company {
+    margin: 0px;
   }
 }
 </style>
